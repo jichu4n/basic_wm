04601a0440de66517cada4f41272212ad744f96b
Chuan Ji <chuan@jichu4n.com>
2024-07-03 16:05:41 -0700

    Improve run script.

--- build_and_run.sh
+++ build_and_run.sh
@@ -12,7 +12,11 @@ make basic_wm
 # We need to specify the full path to Xephyr, as otherwise xinit will not
 # interpret it as an argument specifying the X server to launch and will launch
 # the default X server instead.
-XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
+XEPHYR=$(whereis -b Xephyr | sed -E 's/^.*: ?//')
+if [ -z "$XEPHYR" ]; then
+    echo "Xephyr not found, exiting"
+    exit 1
+fi
 xinit ./xinitrc -- \
     "$XEPHYR" \
         :100 \
